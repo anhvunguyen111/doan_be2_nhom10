d4652584536928181c25cc498c8eee6935340b87
anhvunguyen111 <anhvu4777@gmail.com>
2022-05-20 17:58:45 +0700

    add login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -14,17 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('username');
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('email');
-            $table->string('image');
-            $table->string('phone');
-            $table->string('token_cart');          
-            $table->integer('type_user_id');
-            $table->foreign('type_user_id')->references('id')->on('type_users')->onDelete('cascade');
-            $table->integer('role_id');
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
     }
@@ -38,4 +35,4 @@ class CreateUsersTable extends Migration
     {
         Schema::dropIfExists('users');
     }
-}
+};
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\DetailProductController;
-use App\Http\Controllers\SearchController;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,19 +15,21 @@ use App\Http\Controllers\SearchController;
 |
 */
 
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
-Route::get('/shop-grid/{id}', [ProductController::class, 'index'])->name('shop-grid');
-Route::get('/search', [SearchController::class, 'index'])->name('search');
-Route::get('/{id}', [PageController::class, 'index']);
-Route::get('/', [PageController::class, 'home']);
-Route::get('/shop-details/{id}', [DetailProductController::class, 'index'])->name('shop-details');
-
-
-
-
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth'])->name('dashboard');
-
-// require __DIR__.'/auth.php';
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
